b686b09373033f790df72a82e337e657a7a86505
Jean Paulo dos Santos <jeanpauloeng@gmail.com>
2023-12-21 14:48:31 -0300

    ProjetoDioAvanade

---
+++ Models/Pessoa.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoDio.Models
+{
+    public class Pessoa
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"Olá, meu nome é {Name}, e tenho {Age} anos");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("teste");
+﻿using ProjetoDio.Models;
+
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+// Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+
+// quantidade = 10;
+//  Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+
+// double altura = 1.80;
+
+// decimal preco = 1.80m;
+
+// bool condicao = true;
+
+//Console.WriteLine(apresentacao);
+//Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+//Console.WriteLine("Valor da variavel altura: " + altura);
+//Console.WriteLine("Valor da variavel preco: " + preco);
+//Console.WriteLine("Valor da variavel condicao: " + condicao);
+
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Name = "Jean";
+//pessoa1.Age = 26;
+//pessoa1.Apresentar();
