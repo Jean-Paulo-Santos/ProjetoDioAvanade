ef33def1e013fb3f669c2efe793f066e299a0445
Jean Paulo dos Santos <jeanpauloeng@gmail.com>
2023-12-26 20:41:23 -0300

    Arrays

--- Program.cs
+++ Program.cs
@@ -2,43 +2,36 @@
 using System.Security.Cryptography.X509Certificates;
 using ProjetoDioAvanade.Models;
 
-string opcao;
+List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+listaString.Add("RJ");
 
 
-while(true)
-{
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar Cliente");
-    Console.WriteLine("2 - Buscar Cliente");
-    Console.WriteLine("3 - Apagar Cliente");
-    Console.WriteLine("4 - Encerrar");
+Console.WriteLine($"Percorrendo a lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
 
-    opcao = Console.ReadLine();
+listaString.Add("SC");
 
-    switch(opcao)
-    {
-        case "1":
-            Console.WriteLine("Cadastro de Cliente");
-            break;
+Console.WriteLine($"Percorrendo a lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
 
-        case "2":
-            Console.WriteLine("Busca de Cliente");
-            break;
-        
-        case "3":
-            Console.WriteLine("Apagar Cliente");
-            break;
-        
-        case "4":
-            Console.WriteLine("Encerrar");
-            Environment.Exit(0);
-            break;
+listaString.Add("MG");
 
-        default:
-            Console.WriteLine("Opção invalida");
-            break;
-    }
-}
+Console.WriteLine($"Percorrendo a lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+// for(int contador = 0; contador < listaString.Count; contador++)
+// {
+//     Console.WriteLine($"Posição N° {contador} - {listaString[contador]}");
+// }
+
+// Console.WriteLine("Percorrendo a lista com o FOREACH");
+// int contadorforeach = 0;
+// foreach(string item in listaString)
+// {
+//     Console.WriteLine($"Posição N° {contadorforeach} - {item}");
+//     contadorforeach++;
+// }
 
 
 
@@ -51,6 +44,89 @@ while(true)
 
 
 
+
+
+
+
+
+
+// int[] arrayInteiros = new int[4];
+
+// arrayInteiros[0] = 72;
+// arrayInteiros[1] = 64;
+// arrayInteiros[2] = 50;
+// arrayInteiros[3] = 1;
+
+
+// int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+// Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+
+// //Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+
+// Console.WriteLine("Percorrendo o Array com o FOR");
+// for (int contador = 0; contador < arrayInteiros.Length; contador++)
+// {
+//     Console.WriteLine($"Posição Nº {contador} - {arrayInteiros[contador]}");
+// }
+
+
+
+
+
+// Console.WriteLine("Percorrendo o Array com o FOREACH");
+
+// int contadorForeach = 0;
+
+// foreach (int valor in arrayInteiros)
+// {
+//     Console.WriteLine($"Posição Nº {contadorForeach} - {valor}");
+//     contadorForeach++;
+// }
+
+
+
+
+
+
+// string opcao;
+
+
+// while(true)
+// {
+//     Console.WriteLine("Digite a sua opção:");
+//     Console.WriteLine("1 - Cadastrar Cliente");
+//     Console.WriteLine("2 - Buscar Cliente");
+//     Console.WriteLine("3 - Apagar Cliente");
+//     Console.WriteLine("4 - Encerrar");
+
+//     opcao = Console.ReadLine();
+
+//     switch(opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastro de Cliente");
+//             break;
+
+//         case "2":
+//             Console.WriteLine("Busca de Cliente");
+//             break;
+
+//         case "3":
+//             Console.WriteLine("Apagar Cliente");
+//             break;
+
+//         case "4":
+//             Console.WriteLine("Encerrar");
+//             Environment.Exit(0);
+//             break;
+
+//         default:
+//             Console.WriteLine("Opção invalida");
+//             break;
+//     }
+// }
 
 
 
